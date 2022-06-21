e4998f99583b53f2a0239734020fae62c754520e
PapiaMCode <papiamcode@gmail.com>
2022-06-21 20:42:13 +0600

    Initial Commit

---
+++ CurrencyConverterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CurrencyConverterApp
+//
+//  Created by Jannatun Nahar Papia on 21/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CurrencyConverterApp/CurrencyConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterAppApp.swift
+//  CurrencyConverterApp
+//
+//  Created by Jannatun Nahar Papia on 21/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
